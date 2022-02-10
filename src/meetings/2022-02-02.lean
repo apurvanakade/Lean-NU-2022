1637d58fb8245983906f5bebbefa94bf436e118d
Apurva Nakade <apurvnakade@gmail.com>
2022-02-10 16:57:49 -0600

    Update meeting notes.

--- src/meetings/2022-02-02.lean
+++ src/meetings/2022-02-02.lean
@@ -7,10 +7,13 @@ variables {V : Type u} (G : simple_graph V)
 
 #print simple_graph.walk -- prints the definition
 #check nonempty -- prints the type of the object
+
 -- simple_graph.walk u v  is the type of all walks from u to v
 
 -- prove that if u is adjacent to v then the set of walks from u to v is non-empty
 
+class inductive nonempty' (α : Sort u) : Prop
+| intro (val : α) : nonempty'
 
 theorem adjacent_to_walk {u v : V} (h : G.adj u v) : nonempty (G.walk u v) := 
 begin 
---
+++ src/meetings/2022-02-10.lean
@@ -0,0 +1,37 @@
+-- Next time: let's see how the graph theory theorem about the sum of degrees being even is coded in lean.
+
+import combinatorics.simple_graph.connectivity
+
+universe u
+variables {V : Type u} (G : simple_graph V)
+
+#print simple_graph.walk -- prints the definition
+#check nonempty -- prints the type of the object
+
+-- simple_graph.walk u v  is the type of all walks from u to v
+
+-- prove that if u is adjacent to v then the set of walks from u to v is non-empty
+
+def simple_graph.walks_of_len (G : simple_graph V) (u v : V) (n : ℕ) := { x : G.walk u v // x.length = n }
+
+theorem adjacent_to_walk {u v : V} (h : G.adj u v) : nonempty (G.walks_of_len u v 1) :=
+begin
+  apply nonempty.intro,
+  split, 
+  swap,
+  { have p := @simple_graph.walk.nil _ _ v,
+    exact simple_graph.walk.cons h p },
+  simp only [simple_graph.walk.length_nil, simple_graph.walk.length_cons],
+end
+
+theorem temp' {u v : V} (w : G.walks_of_len u v 0) : u = v :=
+begin 
+  cases w with w h,
+  sorry,
+end 
+
+theorem temp {u v : V} (w : G.walks_of_len u v 1) : G.adj u v := 
+begin 
+  cases w with w h,
+  sorry,
+end
