98986612dd39fe6b533544c0aa8c2461b72cc62a
Apurva Nakade <apurvnakade@gmail.com>
2022-03-10 16:32:08 -0600

    Clean file.

--- src/graph_theory/walks_of_len.lean
+++ src/graph_theory/walks_of_len.lean
@@ -1,64 +1,30 @@
 import combinatorics.simple_graph.connectivity
-import combinatorics.simple_graph.adj_matrix
+import tactic 
+
+open_locale classical
 
 universe u
-variables {V : Type u} [decidable_eq V]
-variables (G : simple_graph V) [decidable_rel G.adj]
-variables {α : Type u} [decidable_eq α] [ring α]
+variables {V : Type u} (G : simple_graph V)
 
-def simple_graph.walks_of_len (G : simple_graph V) (u v : V) (n : ℕ) := 
-  { x : G.walk u v // x.length = n }
 
-theorem walk_of_adj {u v : V} (h : G.adj u v) : nonempty (G.walks_of_len u v 1) :=
-begin
-  apply nonempty.intro,
-  split, 
-  swap,
-  { have p := @simple_graph.walk.nil _ _ v,
-    exact simple_graph.walk.cons h p },
-  simp only [simple_graph.walk.length_nil, simple_graph.walk.length_cons],
-end
-
-theorem eq_of_len_zero {u v : V} (w : G.walks_of_len u v 0) : u = v :=
+theorem eq_of_len_zero {u v : V} (w : G.walk u v) (h : w.length = 0) : u = v :=
 begin 
-  cases w with w h,
-  induction w,
-  { refl },
-  { exfalso,
-    simp only [nat.succ_ne_zero, simple_graph.walk.length_cons] at *,
-    exact h, }
-end 
-
-theorem non_eq_of_len_one {u v : V} (w : G.walks_of_len u v 1) : u ≠ v := 
-begin 
-  cases w with w h,
-  sorry,
-end
-
-theorem exists_walk_of_len {u v : V} (n : ℕ) (w : G.walks_of_len u v (n + 1)) : 
-  ∃ (v' : V) (w' : G.walks_of_len u v' n), (G.adj v' v) := 
-begin 
-  cases w with w h,
-  induction w,
-  sorry,
   sorry,
 end 
 
-theorem adj_of_len_one {u v : V} (w : G.walks_of_len u v 1) : G.adj u v := 
+
+theorem exists_walk_of_len {u v : V} {n : ℕ} (w : G.walk u v) (h : w.length = n + 1) : 
+  ∃ (v' : V) (w' : G.walk u v'), (w'.length = n) ∧ (G.adj v' v) := 
 begin 
-  cases w with w h,
+  -- not sure
   sorry,
-end
+end 
 
-/-- theorems about finiteness -/
-
-theorem walk_finset [fintype V] (u v : V) : finset(simple_graph.walk G u v) :=
+theorem adj_of_len_one {u v : V} (w : G.walk u v) (h : w.length = 1) : G.adj u v := 
 begin 
-  sorry,
-end
-
-
-theorem walks_of_len_finset [fintype V] (n : ℕ) (u v : V) : finset(simple_graph.walks_of_len G u v n) :=
-begin 
-  sorry,
+  have h' := exists_walk_of_len G w h,
+  rcases h' with ⟨v', w', h1, h2⟩,
+  have h3 := eq_of_len_zero G w' h1,
+  rw ← h3 at h2,
+  assumption,
 end
